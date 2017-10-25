fa59986bbb3c970c902b3bca3681d56c82b8602c
Branoli <lar.pasha.chenko@gmail.com>
2017-10-25 17:13:49 +0500

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -4,21 +4,21 @@ class Menu
     public static $Menu = array
     (
         1 => '<html><ul class="styleHeader" class="blockUL">
-                <li class="LOGO"><a href="assets/start.html?page=1">gameName</a></li>
+                <li class="LOGO"><a href="index.php?page=1">gameName</a></li>
                 <li class="aEdit"><a href="">StartGame</a></li>
-                <li class="aEdit"><a href="assets/about.html?page=2">About</a></li>
-                <li class="aEdit"><a href="assets/topList.html?page=3">TopList</a></li>
+                <li class="aEdit"><a href="index.php?page=2">About</a></li>
+                <li class="aEdit"><a href="index.php?page=3">TopList</a></li>
                 </ul></html>',
         2 => '<html><ul class="styleHeader" class="blockUL">
-                <li class="LOGO"><a href="assets/start.html?page=1">gameName</a></li>
-                <li class="aEdit"><a href="assets/start.html?page=2">StartGame</a></li>
+                <li class="LOGO"><a href="index.php?page=1">gameName</a></li>
+                <li class="aEdit"><a href="index.php?page=1">StartGame</a></li>
                 <li class="aEdit"><a href="">About</a></li>
-                <li class="aEdit"><a href="assets/about.html?page=3">TopList</a></li>
+                <li class="aEdit"><a href="index.php?page=3">TopList</a></li>
                 </ul></html>',
         3 => '<html><ul class="styleHeader" class="blockUL">
-                <li class="LOGO"><a href="assets/start.html?page=1">gameName</a></li>
-                <li class="aEdit"><a href="assets/start.html?page=2">StartGame</a></li>
-                <li class="aEdit"><a href="assets/topList.html?page=3">About</a></li>
+                <li class="LOGO"><a href="index.php?page=1">gameName</a></li>
+                <li class="aEdit"><a href="index.php?page=1">StartGame</a></li>
+                <li class="aEdit"><a href="index.php?page=2">About</a></li>
                 <li class="aEdit"><a href="">TopList</a></li>
                 </ul></html>'
     );
